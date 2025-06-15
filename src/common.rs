71dea3467ced0df157f690aa9e730ce6147df437
MRlvb <15244188332@163.com>
2025-06-15 12:18:04 +0800

    修改替换API地址

--- src/common.rs
+++ src/common.rs
@@ -1004,7 +1004,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://120.220.54.221:21114".to_owned()
 }
 
 #[inline]
